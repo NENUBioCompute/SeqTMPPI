22a5c7b22e54ec9171dbc2adc3b3054d48765846
jiangjiuhong <julse@qq.com>
2021-03-19 10:20:35 +0800

    human fasta cd hit

--- _10humanTrain.py
+++ _10humanTrain.py
@@ -7,6 +7,7 @@ import time
 import pandas as pd
 
 from PairDealer import concatPAN
+from _3handleCluster import cluster2Table, pairWithClusterLable, saveRelated
 from _5statistic import findSpecies, relatedSpecies, mergeTwo
 from common import check_path, countline, concatFile
 from dataset import extractPairAndFasta, saveQualified, simplifyTable, getproteinlist
@@ -73,6 +74,7 @@ if __name__ == '__main__':
     check_path(dirout)
     f1qualified_info = os.path.join(dirout, '1qualified_info.tsv')
     f2qualified_info_sample = os.path.join(dirout, '1qualified_info_sample.tsv')
+    f2qualified_info_sample_1 = os.path.join(dirout, '1qualified_info_sample_1.tsv')
 
     fout_fasta = os.path.join(dirout, '2pair.fasta')
     fout_tmp_fasta = os.path.join(dirout, '2tmp.fasta')
@@ -92,19 +94,40 @@ if __name__ == '__main__':
     # mergeTwo(fin_1nega, fin_1tmpInfo, f1qualified_info, left=[0,7], right=[x for x in range(8)],keepright=[x for x in range(7)])
     # mergeTwo(f1qualified_info, fin_1nontmpInfo, f1qualified_info, left=[0,7,1,2,3,4,5,6], right=[x+7 for x in range(8)],keepright=[x+7 for x in range(7)])
 
+
     # sample 1.2 multiple than positive
+    # time 1260.461941242218
 
     # nrows = int(44210 * 1.2)
     # df = pd.read_table(f1qualified_info).sample(nrows)
     # df.to_csv(f2qualified_info_sample,header=None,index=None,sep='\t')
     # print(df.shape,f2qualified_info_sample)
-    # simplifyTable(f2qualified_info_sample, f2positive)
-    # extractPairAndFasta(f2qualified_info_sample, fout_fasta, fout_tmp_fasta=fout_tmp_fasta,
+
+
+
+
+    # reindex
+
+    # left = [0, 7, 1, 2, 3, 4, 5, 6]
+    # right = [x + 7 for x in range(1,7)]
+    # df = pd.read_table(f2qualified_info_sample,header=None)
+    # left.extend(right)
+    # df.columns = left
+    # df = df.reindex(columns=[x for x in range(14)])
+    # df.to_csv(f2qualified_info_sample_1,header=None,index=None,sep='\t')
+
+
+    # save 19456 protein fasta file/10humanTrain/2negative/2pair.fasta
+    # save 5088 tmp  file/10humanTrain/2negative/2tmp.fasta
+    # save 14368 nontmp  file/10humanTrain/2negative/2nontmp.fasta
+
+    # simplifyTable(f2qualified_info_sample_1, f2positive)
+    #
+    # extractPairAndFasta(f2qualified_info_sample_1, fout_fasta, fout_tmp_fasta=fout_tmp_fasta,
     #                     fout_nontmp_fasta=fout_nontmp_fasta)
-    # getproteinlist(f2qualified_info_sample,
+    # getproteinlist(f2qualified_info_sample_1,
     #                ftmp=f2tmp, fnontmp=f2nontmp, fall=f2all,
     #                ftmp_info=f2tmp_info, ftmp_nontmp_info=f2nontmp_info, fall_info=f2all_info)
-
 
     '''
     clusters cd-hit
@@ -143,6 +166,50 @@ if __name__ == '__main__':
 
 
     '''
+    concat 2.0
+    '''
+    f9human_human = 'file/10humanTrain/1positive/9human_human.tsv'
+    f2qualified_info_sample_1 ='file/10humanTrain/2negative/1qualified_info_sample_1.tsv'
+
+    dirout = 'file/10humanTrain/3cluster/all'
+    check_path(dirout)
+
+    fpairinfo = os.path.join(dirout, '1pairinfo.tsv')
+
+    df1 = pd.read_table(f9human_human,header=None)
+    df2 = pd.read_table(f2qualified_info_sample_1,header=None)
+    df3 = pd.concat([df1,df2])
+    df3.to_csv(fpairinfo,header=None,index=None,sep='\t')
+
+    fout_fasta = os.path.join(dirout, '2pair.fasta')
+    fout_tmp_fasta = os.path.join(dirout, '2tmp.fasta')
+    fout_nontmp_fasta = os.path.join(dirout, '2nontmp.fasta')
+    f2positive = os.path.join(dirout, '2pair.tsv')
+    f2tmp = os.path.join(dirout, '2tmp.list')
+    f2nontmp = os.path.join(dirout, '2nontmp.list')
+    f2all = os.path.join(dirout, '2all.list')
+    f2tmp_info = os.path.join(dirout, '2tmp_info.tsv')
+    f2nontmp_info = os.path.join(dirout, '2nontmp_info.tsv')
+    f2all_info = os.path.join(dirout, '2all_info.tsv')
+
+
+    # save 19678 protein fasta file/10humanTrain/3cluster/all/2pair.fasta
+    # save 5089 tmp  file/10humanTrain/3cluster/all/2tmp.fasta
+    # save 14589 nontmp  file/10humanTrain/3cluster/all/2nontmp.fasta
+
+    # simplifyTable(fpairinfo, f2positive)
+    #
+    # extractPairAndFasta(fpairinfo, fout_fasta, fout_tmp_fasta=fout_tmp_fasta,
+    #                     fout_nontmp_fasta=fout_nontmp_fasta)
+    # getproteinlist(fpairinfo,
+    #                ftmp=f2tmp, fnontmp=f2nontmp, fall=f2all,
+    #                ftmp_info=f2tmp_info, ftmp_nontmp_info=f2nontmp_info, fall_info=f2all_info)
+
+
+
+
+
+    '''
     cd hit 0.4 cd-hit tool :http://weizhong-lab.ucsd.edu/cdhit_suite/cgi-bin/index.cgi?cmd=cd-hit
     get  *.clstr file
     '''
@@ -158,7 +225,33 @@ if __name__ == '__main__':
     '''
     extract positive,negative
     '''
-    # saveRelated(f4pair,f4posi,f4nega)
+    # saveRelated(f3pair,f4pair,f4posi,f4nega)
+
+    # '''
+    # combine P and N
+    # '''
+    #
+    # dirin = 'file/3cluster/'
+    # fin_p = os.path.join(dirin, '4posi.tsv')
+    # fin_n = os.path.join(dirin, '4nega.tsv')
+    # fin_fasta = os.path.join(dirin, '1all.fasta')
+    # flist = [fin_p, fin_n]
+    # ratios_pn = [1, 1]
+    # limit = 64939 * 2
+    # # limit = 64939*2/5
+    #
+    # f1out = 'file/4train/'
+    # f2outdir = os.path.join(f1out, str(0))
+    # fin_pair = os.path.join(f2outdir, 'all.txt')
+    #
+    # train = os.path.join(f2outdir, 'train.txt')
+    # validate = os.path.join(f2outdir, 'validate.txt')
+    # test = os.path.join(f2outdir, 'test.txt')
+    # ratios_tvt = [0.8, 0.1, 0.1]
+    # f3outs = [train, validate, test]
+    #
+    # dir_feature_db = '/home/19jjhnenu/Data/SeqTMPPI2W/featuredb/129878/'
+    # dirout_feature = '/home/19jjhnenu/Data/SeqTMPPI2W/feature/129878/'
 
     '''
     group train
@@ -167,7 +260,8 @@ if __name__ == '__main__':
 
 
 
-    # countline(f1qualified_info)
+
+    # countline(f3pair)
     # df = pd.read_table(f4caseStudyPair_onlyOnePDB,header=None)
     # df1.to_csv(fout,header=None,index=None,sep='\t')
 
---
+++ _8DIPPredict.py
@@ -0,0 +1,23 @@
+# Title     : _8DIPPredict.py
+# Created by: julse@qq.com
+# Created on: 2021/3/19 9:12
+# des : TODO
+
+
+import time
+
+if __name__ == '__main__':
+    print('start', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+    start = time.time()
+
+    '''
+    combine negative
+    '''
+    
+
+
+
+    pass
+    print('stop', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+    print('time', time.time() - start)
+
